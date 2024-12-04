42539675af55605c15454ea5e0216d2b59399f4b
GoldmanYT <89934420+GoldmanYT@users.noreply.github.com>
2024-12-04 10:37:18 +0300

    lab 3 - knight cycle

--- lab3/knight_cycle.py
+++ lab3/knight_cycle.py
@@ -6,9 +6,35 @@
 маршруте дважды.
 """
 
+n = 5
+
+
+def on_field(x, y):
+    return 0 <= x < n and 0 <= y < n
+
 
 def main():
-    pass
+    stack = [(0, 0)]
+    next_nodes = [[] for _ in range(n ** 2)]
+    new_node = True
+    count = 0
+    while stack:
+        count += 1
+        x0, y0 = stack[-1]
+        k = len(stack) - 1
+        if new_node:
+            next_nodes[k] = [
+                (x, y) for dx, dy in ((1, 2), (2, 1))
+                for sx in (1, -1) for sy in (1, -1)
+                if on_field(x := x0 + dx * sx, y := y0 + dy * sy)
+                and (x, y) not in stack]
+        if next_nodes[k]:
+            stack.append(next_nodes[k].pop())
+            new_node = True
+        else:
+            stack.pop()
+            new_node = False
+    print(count)
 
 
 if __name__ == '__main__':
