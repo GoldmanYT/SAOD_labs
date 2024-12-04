3531b41a3b3851d7ed94fe3f6952d8945cc743f1
GoldmanYT <89934420+GoldmanYT@users.noreply.github.com>
2024-12-04 11:12:09 +0300

    lab 3 - orders (unfinished)

--- lab3/orders.py
+++ lab3/orders.py
@@ -7,9 +7,11 @@
 выполнения заказов, при котором сумма штрафов будет
 наименьшей.
 """
-
+from math import inf
 from random import randint
 from dataclasses import dataclass
+
+from typing import List
 
 
 @dataclass()
@@ -18,8 +20,8 @@ class Order:
     fine: int
 
 
-def get_orders(n: int) -> list[Order]:
-    a, b = 10, 99
+def get_orders(n: int) -> List[Order]:
+    a, b = 1, 9
     return [Order(
         deadline=randint(a, b), fine=randint(a, b)
     ) for _ in range(n)]
@@ -27,10 +29,38 @@ def get_orders(n: int) -> list[Order]:
 
 def best_order_of_order():
     n = 10
-    orders: list[Order] = get_orders(n)
-    k = 0
-    while k >= 0:
-        pass
+    orders: List[Order] = get_orders(n)
+    next_orders = {}
+    stack: List[Order] = [orders[0]]
+    current_fine = 0
+    best_solution = inf
+    new_order = True
+    for i, order in enumerate(orders, 1):
+        print(f'Заказ №{i}: срок - {order.deadline}, штраф - {order.fine}')
+    while stack:
+        k = len(stack) - 1
+        if len(stack) == n:
+            best_solution = min(best_solution, current_fine)
+        if current_fine >= best_solution:
+            next_orders[k] = []
+        elif new_order:
+            next_orders[k] = [
+                order for order in orders
+                if order not in stack
+            ]
+        if next_orders[k]:
+            new_order = True
+            order = next_orders[k].pop()
+            stack.append(order)
+            if len(stack) > order.deadline:
+                current_fine += order.fine
+        else:
+            new_order = False
+            order = stack.pop()
+            if len(stack) + 1 > order.deadline:
+                current_fine -= order.fine
+    print(current_fine)
+    print(f'Минимальный штраф: {best_solution}')
 
 
 if __name__ == '__main__':
