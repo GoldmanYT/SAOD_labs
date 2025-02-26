113f3c040320e64402792b0e2d21fd95141e9085
Goldman <goldmanmine@mail.ru>
2025-02-26 06:51:43 +0300

    lab 4 - array methods

--- lab4/main.py
+++ lab4/main.py
@@ -13,29 +13,39 @@ class Window:
         self.root = Tk()
         self.root.geometry('1280x720')
 
-        array_methods = [LinearSearch(), BinarySearch()]
+        array_methods = [
+            LinearSearch,
+            BinarySearch,
+            HomogeneousBinarySearch,
+            InterpolationSearch
+        ]
 
         columns = [str(i) for i in range(self.column_count + 1)]
 
-        self.table = ttk.Treeview(self.root, columns=columns, show='headings')
+        self.table = ttk.Treeview(self.root, columns=columns, show='headings', height=20)
         self.table.pack()
 
         self.table.heading('0', text='Метод')
         for i in range(1, self.column_count + 1):
             self.table.heading(str(i), text=f'n = {i * self.step}')
 
-        for method in array_methods:
-            row = [method.name]
+        for method_class in array_methods:
+            compare_count_row = [method_class.name + '\t\tЧС']
+            time_row = ['\t\t\tВРМ']
             for i in range(1, len(columns)):
-                n = 2 * self.step
+                n = self.step * i
+                method = method_class(n=n)
                 array = [i for i in range(2, 2 * n + 1, 2)]
                 for k in range(self.experiment_count):
                     value = randrange(2 - (k < self.experiment_count // 2), 2 * n + 1, 2)
                     method.search(array, value)
-                result = method.get_result()
-                row.append('\t'.join(str(v) for v in result.values()))
+                compare_count = method.get_compare_count()
+                time = method.get_time()
+                compare_count_row.append('\t\t'.join(str(v) for v in compare_count.values()))
+                time_row.append('\t\t'.join(str(v) for v in time.values()))
 
-            self.table.insert('', END, values=row)
+            self.table.insert('', END, values=compare_count_row)
+            self.table.insert('', END, values=time_row)
 
         self.root.mainloop()
 
--- lab4/methods.py
+++ lab4/methods.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
+from math import log2, floor
 
-to_mcs = 1_000_000
+to_ns = 1_000_000_000
 
 
 def timer(search):
@@ -9,7 +10,7 @@ def timer(search):
         start_time = datetime.now()
         success = search(self, array, value)
         end_time = datetime.now()
-        time = int(to_mcs * (end_time - start_time).total_seconds())
+        time = round(to_ns * (end_time - start_time).total_seconds())
         if success:
             self.time_success += time
             self.count_success += 1
@@ -23,7 +24,7 @@ def timer(search):
 class ArraySearchMethod(ABC):
     name = None
 
-    def __init__(self):
+    def __init__(self, *_, **__):
         self.count_compare_fail = 0
         self.count_compare_success = 0
         self.count_fail = 0
@@ -44,10 +45,18 @@ class ArraySearchMethod(ABC):
         pass
 
     def get_result(self):
-        return {'count_compare_fail': self.count_compare_fail // self.count_fail,
-                'count_compare_success': self.count_compare_success // self.count_success,
-                'time_fail': self.time_fail // self.count_fail,
-                'time_success': self.time_success // self.count_success}
+        return {'count_compare_success': round(self.count_compare_success / self.count_success),
+                'count_compare_fail': round(self.count_compare_fail / self.count_fail),
+                'time_success': round(self.time_success / self.count_success),
+                'time_fail': round(self.time_fail / self.count_fail)}
+
+    def get_compare_count(self):
+        return {'count_compare_success': round(self.count_compare_success / self.count_success),
+                'count_compare_fail': round(self.count_compare_fail / self.count_fail)}
+
+    def get_time(self):
+        return {'time_success': round(self.time_success / self.count_success),
+                'time_fail': round(self.time_fail / self.count_fail)}
 
     def __str__(self):
         return str(self.get_result())
@@ -88,5 +97,66 @@ class BinarySearch(ArraySearchMethod):
             else:
                 compare_count += 3
                 return self.success(compare_count)
+
+        return self.fail(compare_count)
+
+
+class HomogeneousBinarySearch(ArraySearchMethod):
+    name = 'Однородный бинарный поиск'
+
+    def __init__(self, n, *_, **__):
+        super().__init__()
+        self.delta = [floor((n + 2 ** j) / 2 ** (j + 1))
+                      for j in range(floor(log2(n)) + 2)]
+
+    @timer
+    def search(self, array, value):
+        compare_count = 0
+        m = self.delta[0]
+        j = 0
+
+        while self.delta[j] != 0:
+            j += 1
+            if array[m] > value:
+                compare_count += 1
+                m -= self.delta[j]
+            elif array[m] < value:
+                compare_count += 2
+                m += self.delta[j]
+            else:
+                compare_count += 3
+                return self.success(compare_count)
+
+        return self.fail(compare_count)
+
+
+class InterpolationSearch(ArraySearchMethod):
+    name = 'Интерполяционный поиск'
+
+    @timer
+    def search(self, array, value):
+        compare_count = 0
+        left = 0
+        right = len(array) - 1
+
+        while left <= right:
+            mid = left + floor((right - left) * (value - array[left]) / (array[right] - array[left]))
+            if array[mid] > value:
+                compare_count += 1
+                right = mid - 1
+            elif array[mid] < value:
+                compare_count += 2
+                left = mid + 1
+            else:
+                compare_count += 3
+                return self.success(compare_count)
+
+            if array[left] > value:
+                compare_count += 1
+                break
+            elif array[right] < value:
+                compare_count += 2
+                break
+            compare_count += 2
 
         return self.fail(compare_count)
